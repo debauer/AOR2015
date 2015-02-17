7fb5725ad4443ab92993e99bafbbacd11cb4317d
David Bauer <me@debauer.net>
2015-02-17 17:12:12 +0100

    update lcd from rpi tuut

--- py/din_daemon.py
+++ py/din_daemon.py
@@ -4,11 +4,14 @@ import time
 
 print psutil.cpu_times()
 
-ser = serial.Serial('/dev/ttyACM0', 19200, timeout=1)
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+
 
 while 1:
-	time.sleep(5)
+	time.sleep(1)
 	value = psutil.cpu_percent()
-	ser.write("value 0" + str(value))
-	print "value 0 " + str(value)
+	title = run_cmd('mpc current')[15:]
+	ser.write("string 0 "+title+ " \r")
+	ser.write("value 0 " + str(value) + " \r")
+	print "value 0 " + str(value) + "\r"
 ser.close()
--- scripts/sync.cmd
+++ scripts/sync.cmd
@@ -1,2 +1,2 @@
 $PATH = /home/AOR2015
-rsync --numeric-ids -avze ssh ../* root@%1:/home/AOR2015
+rsync --delete --numeric-ids -avze ssh ../* root@%1:/home/AOR2015
