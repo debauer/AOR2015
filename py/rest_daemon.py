632f97a2bd7391efab27cec1a5f5f6aa334a7ac4
root <root@raspberrypi.(none)>
2015-03-23 16:21:10 +0000

    morbi <3

--- py/rest_daemon.py
+++ py/rest_daemon.py
@@ -5,16 +5,81 @@
 from flask import Flask, jsonify
 import pymongo
 from pymongo import MongoClient
+#from elasticsearch import Elasticsearch
 from flask import request
 from bson import BSON
 from bson import json_util
+import time
+from datetime import datetime
+#from influxdb import InfluxDBClient
+from influxdb.influxdb08 import InfluxDBClient
+import psutil
+import json
+from thread import start_new_thread
+from termcolor import colored
 
 mongo = MongoClient()
 #mongo = MongoClient('mongodb://localhost:27017/')
 
-db = mongo.rallye
-values = db.values
+config = json.loads(open('config.json').read())
+db_influx = InfluxDBClient(config['influx']['host'],
+	config['influx']['port'],
+	config['influx']['user'],
+	config['influx']['pw'],
+	config['influx']['db'])
+
+db_mongo = mongo.rallye
+values = db_mongo.values
 app = Flask(__name__)
+#es = Elasticsearch()
+
+hostname = config['hostname']
+
+#es.indices.create(index='system-index', ignore=400)
+
+def print_logger(str):
+	print colored("logger:", 'cyan'), colored(str, 'magenta')
+
+def log_cpu():
+	cpu = psutil.cpu_percent(interval=1)
+	#res = es.index(index="fileserv-index", doc_type='cpu', body={
+    #	"name": "cpu_idle",
+    #	"value": cpu,
+    #	"@timestamp": datetime.utcnow()
+	#})
+	db_influx.write_points([{
+		"name": "cpu_idle",
+		"columns": ["value", "host"],
+		"points": [
+		[cpu, hostname]
+		]
+	}])
+	print_logger("CPU -> val:" + str(cpu))
+
+def log_mem():
+	mem = psutil.virtual_memory()
+	#res = es.index(index="fileserv-index", doc_type='memory', body={
+    #	"name": "mem_usage",
+    #	"used": mem.used/1024/1024,
+    #	"total": mem.total/1024/1024,
+    #	"percent": mem.percent/1024/1024,
+    #	"@timestamp": datetime.utcnow()
+	#})
+	db_influx.write_points([{
+		"name": "mem_usage",
+		"columns": ["used", "total", "percent", "host"],
+		"points": [
+		[mem.used, mem.total, mem.percent, hostname]
+		]
+	}])
+	print_logger("RAM -> used:" + str(mem.used/1024/1024) + "MB")
+
+def thread_logger():
+	while(1):
+		log_cpu()
+		log_mem()
+		time.sleep(5)
+
 
 # curl -i http://127.0.0.1:5002/api/v1.0/value
 @app.route('/api/v1.0/ping', methods=['get'])
@@ -50,5 +115,11 @@ def post_value():
 	return json_util.dumps(doc, sort_keys=True, indent=4, default=json_util.default), 201
 
 if __name__ == '__main__':
+	start_new_thread(thread_logger,())
+
 	#app.run(host="192.168.3.184",port=5002,debug=True)
 	app.run(port=5002,debug=True)
+
+	#!/usr/bin/python
+
+
--- tutorials/basic_install.sh
+++ tutorials/basic_install.sh
@@ -6,46 +6,71 @@
 	sudo su
 	cd 
 	cat /home/pi/.bashrc >> .bashrc
-	aptitude install htop screen mc
+	aptitude -y install htop screen mc python-pip
+	adduser debauer
 
-# elasticsearch 1.4.4 - tested on rpi2
+# elasticsearch 1.4.4 - tested on rpi2 - wird erstmal nicht mehr gebraucht
 
 	mkdir /usr/share/elasticsearch
 	cd /usr/share/elasticsearch
 	curl -L -O https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.4.tar.gz
 	
-	nano config/elasticsearch.yml
-	# edit 
-	# #path.data: /path/to/data -> path.data: /mnt/ssd0/elasticdata
+	echo "path.data: /mnt/ssd0/elasticdata" > config/elasticsearch.yml
 	
-	# in screen
-	cd elasticsearch-1.4.4/bin
-	./elasticsearch
+	#mv elasticsearch-1.4.4 elasticsearch
+	cd bin
+	./elasticsearch -d 
+	echo "@reboot /usr/share/elasticsearch/bin/elasticsearch -d" > /etc/crontab
 
 # mongodb 2.1.1 - tested on rpi2
 # https://github.com/RickP/mongopi
+	cd /home/pi
 	git clone https://github.com/svvitale/mongo4pi
 	cd mongo4pi
 	./install.sh
 
 # nodejs v0.10.37 - tested on rpi2
 	curl -sL https://deb.nodesource.com/setup | sudo bash -
-	sudo apt-get install -y build-essential python-dev python-rpi.gpio nodejs
+	aptitude -y install build-essential python-dev python-rpi.gpio nodejs
 	
 
 # samba
-	aptitude install samba samba-common-bin
-	nano /etc/samba/smb.conf
-	# edit 
-	# #security = user -> security = user
+	aptitude -y install samba samba-common-bin
 
-#echo "deb http://mirrordirector.raspbian.org/raspbian/ jessie main contrib non-free rpi" > /etc/apt/sources.list.d/jessie.list
+	echo "security = user" > nano /etc/samba/smb.conf
+	sed -i 's/#security = user/security = user/g' /etc/samba/smb.conf
+	echo "[debauer]" > /etc/samba/smb.conf
+	echo "path = /home/debauer" > /etc/samba/smb.conf
+	echo "writeable = yes" > /etc/samba/smb.conf
+	echo "guest ok  = no" > /etc/samba/smb.conf
 
-# sudo aptitude install build-essential libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-thread-dev scons libboost-all-dev python-pymongo git
+	cd /home/debauer
+	git clone https://github.com/debauer/AOR2015
+	chown debauer:debauer * -R
 
+# aor_daemon
+	cd /home/debauer/AOR2015/py
+	pip install -r requirements.txt
 
-#git clone https://github.com/4commerce-technologies-AG/meteor.git
-#cd meteor
-#cp -a /bin/. /usr/bin/
-#cp -a /lib/. /usr/lib
-#cp -a /include/. /usr/include
+# apache 2
+	aptitude install apache2
+
+# graphite - verworfen
+#	cd /home/debauer/AOR2015/py
+#	pip install -r requirements.txt
+#	cd /opt/graphite/conf/
+#   ... 
+  
+# influxdb
+	cd /home/pi
+	wget http://demos.pihomeserver.fr/influxdb_0.8.6_armhf.deb
+	dpkg --install influxdb_0.8.6_armhf.deb
+	service influxdb start
+
+# grafana
+	cd /var/www
+	wget http://grafanarel.s3.amazonaws.com/grafana-1.9.1.tar.gz
+	tar -xzf grafana-1.9.1.tar.gz grafana-1.9.1
+	mv grafana-1.9.1 grafana
+	cd grafana
+	cat /home/debauer/AOR2015/configs/grafana.js >> config.js
