0c07008db48d65b6da2e6c092a685312c52f4d3e
debauer <me@debauer.net>
2015-04-11 18:28:51 +0000

    asd

--- py/mc_daemon.py
+++ py/mc_daemon.py
@@ -2,16 +2,13 @@
 #
 #music and control daemon
 
-import psutil
-import json
-import serial
-import time
 from subprocess import *
-import os
+import os,sys,time,serial,json,psutil
 import pymongo
 from pymongo import MongoClient
 from bson import BSON
 from bson import json_util
+import RPi.GPIO as GPIO
 
 config = json.loads(open('../configs/aor_service.json').read())
 
@@ -29,6 +26,25 @@ telegram_sleep = float(config['telegram_sleep'])
 use_serial = True
 
 lcd_seite = 0
+
+GPIO.setmode(GPIO.BOARD)
+#GPIO.setwarnings(False) 
+
+ENCODER_1 		= 33
+ENCODER_2 		= 35
+BUTTON_ENCODER  = 37
+BUTTON_NEXT		= 40
+BUTTON_PREV		= 38
+BUTTON_UP		= 22
+BUTTON_DOWN		= 18
+
+GPIO.setup(ENCODER_1, 		GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(ENCODER_2, 		GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(BUTTON_ENCODER, 	GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(BUTTON_NEXT, 	GPIO.IN, pull_up_down = GPIO.PUD_UP) 
+GPIO.setup(BUTTON_PREV,		GPIO.IN, pull_up_down = GPIO.PUD_UP) 
+GPIO.setup(BUTTON_UP, 		GPIO.IN, pull_up_down = GPIO.PUD_UP) 
+GPIO.setup(BUTTON_DOWN, 	GPIO.IN, pull_up_down = GPIO.PUD_UP) 
 
 mpc = {}
 
@@ -107,6 +123,11 @@ def mpc_get_status2():
 	status = {"volume": l[7:10],"repeat": l[22:25],"random": l[36:39],"single": l[50:53],"consume": l[65:68],"position": "xx/xx","title": ""}
  	return status
 
+def cleanup():
+	if(use_serial):
+		ser.close()
+	GPIO.cleanup()
+
 def mpc_get_playlist(inc,length = 4):
 	p =  mpc_cmd('playlist')
 	playlist = p.split('\n')
@@ -143,13 +164,12 @@ def serial_send_mpd(id):
 			serial_write("mpd 0 "+ mpc["song"] + " \r")
 	if(song_status != mpc["song_status"] or id == -2):
 		mpc["song_status"] = song_status
-		#if(id == 1 or id < 0):
-		#	serial_write("mpd 1 "+ mpc["song_status"]["title"] + " \r")
+		if(id == 1 or id < 0):
+			serial_write("mpd 1 "+ mpc["song_status"]["title"] + " \r")
 	if(status != mpc["status"] or id == -2):
 		if(id == 2 or id < 0):
 			mpc["status"] = status
 			serial_write("mpd 2 V: " + mpc["status"]["volume"] + "%  RE: " + mpc["status"]["repeat"] +"  RE: " + mpc["status"]["random"] +"\r")
-
 
 def serial_send_values():
 	if(store_mongo):
@@ -157,22 +177,40 @@ def serial_send_values():
 		temperatur_format = "{:4.1f}".format(temperatur)
 		serial_write("value 2 "+ temperatur_format + " \r")
 
+def mpc_next(channel):
+	mpc_cmd("next")
+	print("Button next pressed")
 
-mpc["song_status"] = mpc_get_status1()
-mpc["status"] = mpc_get_status2()
-mpc["song"] = mpc_get_song()[:24]
-serial_send_mpd(-2)
-while 1:
-	#print mpc_get_status2()
-	#print mpc_get_song()
-	#print mpc_get_stats()
-	time.sleep(circle_sleep)
-	if lcd_seite == 0:
-		# |   artist 1 - song 123    |
-		# |  #12/24 1:24/4:31 (34%)  |
-		# | V: 51%  RE: off  RA: off |
-		serial_send_values()
-		serial_send_mpd(-1)
+def mpc_prev(channel):
+	mpc_cmd("prev")
+	print("Button prev pressed")
 
-if(use_serial):
-	ser.close()
+GPIO.add_event_detect(BUTTON_NEXT, GPIO.FALLING, callback=mpc_next, bouncetime=300)
+GPIO.add_event_detect(BUTTON_PREV, GPIO.FALLING, callback=mpc_prev, bouncetime=300)
+
+try:
+	mpc["song_status"] = mpc_get_status1()
+	mpc["status"] = mpc_get_status2()
+	mpc["song"] = mpc_get_song()[:24]
+	serial_send_mpd(-2)
+	while 1:
+		#print mpc_get_status2()
+		#print mpc_get_song()
+		#print mpc_get_stats()
+		time.sleep(circle_sleep)
+		if lcd_seite == 0:
+			# |   artist 1 - song 123    |
+			# |  #12/24 1:24/4:31 (34%)  |
+			# | V: 51%  RE: off  RA: off |
+			serial_send_values()
+			serial_send_mpd(-1)
+
+except KeyboardInterrupt:
+	print "KeyboardInterrupt"
+
+except:
+	print sys.exc_info()
+	print "other error"
+
+finally:
+    cleanup()
