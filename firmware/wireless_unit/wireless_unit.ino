e03a84932293703708c44d631e87c20e33cf2778
David Bauer <me@debauer.net>
2015-06-26 10:31:03 +0200

    asd

--- firmware/wireless_unit/wireless_unit.ino
+++ firmware/wireless_unit/wireless_unit.ino
@@ -22,18 +22,37 @@
 #include "ESP8266.h"
 #include <SoftwareSerial.h>
 
-// @schlager
-#define SSID        "FRITZ!Box Fon WLAN 7390"
-#define PASSWORD    "9089879043196882" 
-#define HOST_NAME   "192.168.3.184"    
-#define HOST_PORT   (5002)
+#define FABLAB
 
-SoftwareSerial mySerial(4, 5); /* RX:D3, TX:D2 */
-ESP8266 wifi(mySerial);
+#ifdef FABLAB
+  #define SSID        "Fablab Karlsruhe"
+  #define PASSWORD    "foobar42" 
+  #define HOST_NAME   "192.168.1.6"    
+  #define HOST_PORT   (5002)
+#endif
+
+#ifdef SCHLAGER
+  // @schlager
+  #define SSID        "FRITZ!Box Fon WLAN 7390"
+  #define PASSWORD    "9089879043196882" 
+  #define HOST_NAME   "192.168.3.184"    
+  #define HOST_PORT   (5002)
+#endif
+
+#ifdef RALLYE
+  // @schlager
+  #define SSID        "AOR 2015"
+  #define PASSWORD    "AOR 2015" 
+  #define HOST_NAME   "192.168.4.6"    
+  #define HOST_PORT   (5002)
+#endif
+
+//SoftwareSerial mySerial(4, 5); /* RX:D3, TX:D2 */
+ESP8266 wifi(Serial1,115200);
 
 void setup(void)
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
     Serial.print("setup begin\r\n");
     
     Serial.print("FW Version:");
@@ -65,31 +84,45 @@ void setup(void)
 void loop(void)
 {
     uint8_t buffer[128] = {0};
-    
+    uint32_t len = 0;
     if (wifi.createTCP(HOST_NAME, HOST_PORT)) {
         Serial.print("create tcp ok\r\n");
     } else {
         Serial.print("create tcp err\r\n");
     }
-    
-    char *hello = "Hello, this is client!";
-    wifi.send((const uint8_t*)hello, strlen(hello));
-    
-    uint32_t len = wifi.recv(buffer, sizeof(buffer), 10000);
-    if (len > 0) {
-        Serial.print("Received:[");
-        for(uint32_t i = 0; i < len; i++) {
-            Serial.print((char)buffer[i]);
+
+        char *hello = "GET /api/v1.0/ping HTTP/1.1\r\n\r\n";
+        Serial.print("Sende:"); 
+        Serial.println(hello);
+        if(wifi.send((const uint8_t*)hello, strlen(hello))){
+              len = 0;
+              Serial.println("Senden OK"); 
+              len = wifi.recv(buffer, sizeof(buffer),5000);
+              Serial.print("Status Bytes: ");
+              Serial.println(len);
+              if (len > 0) {
+                   Serial.print("empfangener String: ");
+                   for(uint32_t i = 0; i < len; i++) {
+                       Serial.print((char)buffer[i]);
+                   }
+                    len = wifi.recv(buffer, sizeof(buffer),5000);
+                    Serial.print("Payload Bytes: ");
+                    Serial.println(len);
+                    if (len > 0) {
+                         Serial.print("empfangener String: ");
+                         for(uint32_t i = 0; i < len; i++) {
+                             Serial.print((char)buffer[i]);
+                         }
+                    }
+              }
         }
-        Serial.print("]\r\n");
-    }
     
-    if (wifi.releaseTCP()) {
-        Serial.print("release tcp ok\r\n");
-    } else {
-        Serial.print("release tcp err\r\n");
-    }
-    delay(1000);
+      if (wifi.releaseTCP()) {
+          Serial.print("release tcp ok\r\n");
+      } else {
+          Serial.print("release tcp err\r\n");
+      }
+    delay(500);
 }
      
 
